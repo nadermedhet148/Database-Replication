45f8f4b5127c0ea624b630d6eef4c907a7b6420b
nader <nader@192.168.1.12>
2021-01-09 13:32:24 +0200

    [FEAT] Master slave replication

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Database-Replication
+# Database Replication :
+__________________________
+
+## Master slave replication :
+____________________________________
+Master-slave replication enables data from one database server (the master) to be replicated to one or more other database servers (the slaves). The master logs the updates, which then ripple through to the slaves. The slave outputs a message stating that it has received the update successfully, thus allowing the sending of subsequent updates. Master-slave replication can be either synchronous or asynchronous. The difference is simply the timing of propagation of changes. If the changes are made to the master and slave at the same time, it is synchronous. If changes are queued up and written later, it is asynchronous.
+![MasterSlaveReplication](docs/dbreplication173.png)
---
+++ master slave replication/main.py
@@ -0,0 +1,99 @@
+import mysql.connector
+import concurrent.futures
+import threading
+import random
+
+leader = {
+    "connection": mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Admin1234",
+        port=6603,
+        database="con3"
+    ),
+    "replications": [
+        mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Admin1234",
+            port=6603,
+            database="con4"
+        ),
+        mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Admin1234",
+            port=6603,
+            database="con2"
+        ),
+        mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Admin1234",
+            port=6603,
+            database="con1"
+        ),
+    ]
+}
+
+class PostDb:
+    def __init__(self):
+        pass
+
+    def insertPost(self,name):
+        connection = leader.get('connection')
+        mycursor = connection.cursor()
+
+        sql = "INSERT INTO post (name) VALUES (%s)"
+        val = (name, )
+        mycursor.execute(sql, val)
+
+        connection.commit()
+        print(mycursor._insert_id , " - record inserted with name : " , name)
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = []
+            for connection in leader.get('replications'):
+                futures.append(executor.submit(self.__insertPostToReplica, id = mycursor._insert_id,  name=name,connection=connection))
+
+
+    def readPost(self,name):
+        connection = random.choice(leader.get('replications'))
+
+
+        mycursor = connection.cursor()
+
+        sql = "Select * from post where name = %s"
+        val = (name,)
+        mycursor.execute(sql, val)
+
+        myresult = mycursor.fetchall()
+
+        for x in myresult:
+            print(x)
+
+# we send the id with the replice to avoid requests latency to override the id 
+    def __insertPostToReplica(self,name,id,connection) :
+        mycursor = connection.cursor()
+
+        sql = "INSERT INTO post (id,name) VALUES (%s,%s)"
+        val = (id,name, )
+        mycursor.execute(sql, val)
+
+        connection.commit()
+        print(mycursor._insert_id , " - record inserted with name : " , name)
+
+
+
+postDb = PostDb()
+jobs = []
+
+postDb.insertPost('name')
+postDb.readPost('name')
+
+postDb.insertPost('name2')
+postDb.readPost('name2')
+
+postDb.insertPost('name3')
+postDb.readPost('name3')
+
